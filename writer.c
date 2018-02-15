7b9c2dfa4958dd426cc88a8b98025ddfb783b6d2
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2018-02-14 20:41:48 -0500

    Writer working? Untested.

--- writer.c
+++ writer.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ipc.h>
@@ -16,9 +17,14 @@
 
 int main (int argc, char **argv)
 {
+    key_t shmKey;
+    char *path = "/Users/Josh/key";
 	int shmId;
 	int size = 4096;
 	char *shmPtr;
+    
+    /* Create the key. */
+    shmKey = ftok (path, 'x');
 
 	/* Create the shared memory segment. */	
 	if ((shmId = shmget (IPC_PRIVATE, size, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0)
@@ -34,6 +40,20 @@ int main (int argc, char **argv)
 		exit (1);
 	}
 
-	
+    printf("Enter a message: ");
+    char msg[32];
+    fgets (msg, 32, stdin);
+    
+    /* Remove trailing new line. */
+    int len = strlen (msg);
+    if (msg[len - 1] == '\n')
+    {
+        msg[len - 1] = '\0';
+    }
+    
+    /* Write to shared memory. */
+    printf ("Writing message '%s' to shared memory...\n", msg);
+    memcpy (shmPtr, msg, sizeof(msg));
  	
+    /* Deallocate after done accepting input. */
 }
